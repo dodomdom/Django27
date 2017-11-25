dcd3549ef267cc00f11b2e0fce857b74ccdb8200
dodomdom <dodomdom412@gmail.com>
2017-11-26 01:20:29 +0900

    완료

--- Django/Mysite/comments/urls.py
+++ Django/Mysite/comments/urls.py
@@ -21,10 +21,11 @@ from django.contrib import admin
 
 from .views import (
     comment_thread,
+    comment_delete,
     )
 
 urlpatterns = [
     url(r'^(?P<id>\d+)/$', comment_thread, name='thread'),
-    #url(r'^post/(?P<id>\d+)/delete/$', comment_delete, name='cdelete'),
+    url(r'^(?P<id>\d+)/delete/$', comment_delete, name='c_delete'),    
 ]
 
--- Django/Mysite/comments/views.py
+++ Django/Mysite/comments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from .forms import CommentForm
 from django.contrib.contenttypes.models import ContentType
 from .models import Comment
@@ -43,3 +43,17 @@ def comment_thread(request, id):
 		"comment": obj,
 	}
 	return render(request,"comment_thread.html",context)
+
+def comment_delete(request, id):
+	obj = get_object_or_404(Comment, id=id)
+	if (request.user == obj.user) or request.user.is_staff or request.user.is_superuser:
+		if request.method == "POST":
+			parent_obj_url = obj.content_object.get_absolute_url()
+			obj.delete()
+			return HttpResponseRedirect(parent_obj_url)
+		context = {
+			"object": obj
+		}
+		return render(request, "confirm_delete.html", context)
+	else:
+		return redirect("/login/")
